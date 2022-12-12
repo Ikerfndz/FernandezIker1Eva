293f023a598582ac140f290b7e4b49489366d97b
Ikerfndz <jeu39212@educastur.es>
2022-12-12 20:01:18 +0100

    jcbjs

--- app/src/main/java/com/example/fernandeziker1eva/MainActivity.java
+++ app/src/main/java/com/example/fernandeziker1eva/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         // animacion
          txt = findViewById(R.id.textoNombre);
